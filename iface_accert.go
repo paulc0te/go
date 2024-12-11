9f6bff9d188b3bc4fa1f8d1535e920b65de0b762
Paul Cote <notfound440@gmail.com>
2024-12-11 07:35:10 +0100

    Create iface_accert.go

---
+++ iface_accert.go
@@ -0,0 +1,52 @@
+package main
+
+func getExpenseReport(e expense) (string, float64) {
+	// ?
+	varEmail, ok := e.(email)
+	if ok {
+		return varEmail.toAddress, e.cost()
+	}
+	varSms, ok := e.(sms)
+	if ok {
+		return varSms.toPhoneNumber, e.cost()
+	}
+	return "", .0
+}
+
+// don't touch below this line
+
+type expense interface {
+	cost() float64
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
+}
