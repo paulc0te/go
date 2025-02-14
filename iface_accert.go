97f1bfd962e808fc50b9a8fa716f763ecbd3721f
Paul Cote <notfound440@gmail.com>
2025-02-14 07:13:53 +0100

    Update iface_accert.go

--- iface_accert.go
+++ iface_accert.go
@@ -40,12 +40,6 @@ func (e email) cost() float64 {
 	return float64(len(e.body)) * .01
 }
 
-func (e email2) cost() float64 {
-	if !e.isSubscribed {
-		return float64(len(e.body)) * .05
-	}
-	return float64(len(e.body)) * .01
-}
 
 
 
