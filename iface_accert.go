264bf546ea8ef32021503b0b96163ad486e127b7
Paul Cote <alexcorpit@gmail.com>
2025-04-26 10:19:50 +0200

    Update iface_accert.go

--- iface_accert.go
+++ iface_accert.go
@@ -47,9 +47,4 @@ func (e email) cost() float64 {
 		return float64(len(e.body)) * .01
 
 		return float64(len(e.body)) * .01
-
-func (s sms) cost() float64 {
-	if !s.isSubscribed {
-		return float64(len(s.body)) * .1
-	}
 	
